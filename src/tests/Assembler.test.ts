36a5acbfc30c3d7d7f0551fa5dc2154a87062175
Jan Mietzelfeld <jan-luca.mietzelfeld@stud.hs-hannover.de>
2026-05-09 12:15:15 +0200

    Better operand type encoding

--- src/tests/Assembler.test.ts
+++ src/tests/Assembler.test.ts
@@ -8,7 +8,7 @@ describe('Encode instructions', () => {
     test('Encode instruction "ADD $1, %eax"', () => {
         const result: DoubleWord[] = assembler.assemble("ADD $1, %eax");
         const expectedOutput: DoubleWord[] = [
-            DoubleWord.fromNumber(0b00000000_00011010_00000001_00000000),
+            DoubleWord.fromNumber(0b00000000_00100011_00000001_00000000),
         ];
         expect(result).toEqual(expectedOutput);
     });
@@ -16,7 +16,7 @@ describe('Encode instructions', () => {
     test('Encode instruction "MOV $0x64, %eax"', () => {
         const result: DoubleWord[] = assembler.assemble("MOV $0x64, %eax");
         const expectedOutput: DoubleWord[] = [
-            DoubleWord.fromNumber(0b00010010_00011010_01100100_00000000),
+            DoubleWord.fromNumber(0b00010010_00100011_01100100_00000000),
         ];
         expect(result).toEqual(expectedOutput);
     });
@@ -24,7 +24,7 @@ describe('Encode instructions', () => {
     test('Encode instruction "NOP"', () => {
         const result: DoubleWord[] = assembler.assemble("NOP");
         const expectedOutput: DoubleWord[] = [
-            DoubleWord.fromNumber(0b11111111_10001000_00000000_00000000),
+            DoubleWord.fromNumber(0b11111111_00010001_00000000_00000000),
         ];
         expect(result).toEqual(expectedOutput);
     });
@@ -32,7 +32,7 @@ describe('Encode instructions', () => {
     test('Encode instruction "ADD" with negative decimal immediate', () => {
         const result: DoubleWord[] = assembler.assemble("ADD $-10, %eax");
         const expectedOutput: DoubleWord[] = [
-            DoubleWord.fromNumber(0b00000000_10011010_00000000_00000000),
+            DoubleWord.fromNumber(0b00000000_10100011_00000000_00000000),
             DoubleWord.fromNumber(0b11111111111111111111111111110110),
         ];
         expect(result).toEqual(expectedOutput);
@@ -41,7 +41,7 @@ describe('Encode instructions', () => {
     test('Encode instruction "ADD" with negative hexadecimal immediate', () => {
         const result: DoubleWord[] = assembler.assemble("ADD $-0x10, %eax");
         const expectedOutput: DoubleWord[] = [
-            DoubleWord.fromNumber(0b00000000_10011010_00000000_00000000),
+            DoubleWord.fromNumber(0b00000000_10100011_00000000_00000000),
             DoubleWord.fromNumber(0b11111111111111111111111111110000),
         ];
         expect(result).toEqual(expectedOutput);
@@ -50,7 +50,7 @@ describe('Encode instructions', () => {
     test('Encode instruction "ADD" with negative binary immediate', () => {
         const result: DoubleWord[] = assembler.assemble("ADD $-0b10, %eax");
         const expectedOutput: DoubleWord[] = [
-            DoubleWord.fromNumber(0b00000000_10011010_00000000_00000000),
+            DoubleWord.fromNumber(0b00000000_10100011_00000000_00000000),
             DoubleWord.fromNumber(0b11111111111111111111111111111110)
         ];
         expect(result).toEqual(expectedOutput);
@@ -59,21 +59,21 @@ describe('Encode instructions', () => {
     test("Encode assembly programs", () => {        
         const result: DoubleWord[] = assembler.assemble(readFileSync("./os_filesystem/home/examples/loop.asm", "utf8"));
         const expectedOutput: DoubleWord[] = [
-            DoubleWord.fromNumber(0x121A6400),
+            DoubleWord.fromNumber(0x12236400),
 
-            DoubleWord.fromNumber(35258624),
+            DoubleWord.fromNumber(35848448),
 
-            DoubleWord.fromNumber(119144448),
+            DoubleWord.fromNumber(119734272),
 
-            DoubleWord.fromNumber(247988224),
+            DoubleWord.fromNumber(248578048),
             DoubleWord.fromNumber(4),
 
-            DoubleWord.fromNumber(312082435),
+            DoubleWord.fromNumber(312672259),
             DoubleWord.fromNumber(305419896),
 
-            DoubleWord.fromNumber(303698176),
+            DoubleWord.fromNumber(304288000),
             
-            DoubleWord.fromNumber(454590464),
+            DoubleWord.fromNumber(455180288),
         ];
         expect(result).toEqual(expectedOutput);
     });
